2602db2be2b6fddadf762b24a922447eea10a7d6
Diego Moncada <tank_mac@Tests-MacBook-Pro.local>
2018-06-08 13:03:53 -0400

    created and finished person class

---
+++ src/app/person.ts
@@ -0,0 +1,26 @@
+export class Person {
+    id:number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+    constructor(id:number, firstName:string, lastName:string, dateOfBirth:Date){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    getAge(): number{
+        let timeRange: number = (new Date().getTime() - this.dateOfBirth.getTime());
+        return Math.floor(timeRange/31556952000);
+
+        // let today: Date = new Date();
+        // let age: number = today.getFullYear() - this.dateOfBirth.getFullYear();
+        // if(this.dateOfBirth.getDate()>today.getDate() &&
+        //     this.dateOfBirth.getMonth()>today.getMonth()){
+        //     age--;
+        // }
+        // return age;
+    }
+}
