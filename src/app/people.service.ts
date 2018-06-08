26ab56b15f8af9a8dd673b214cb537bc693f7e48
Diego Moncada <tank_mac@Tests-MacBook-Pro.local>
2018-06-08 13:04:22 -0400

    added service with a person array

---
+++ src/app/people.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Person } from './person';
+import { Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+
+  people: Person[] = [
+    new Person(1,'Bill','Wright',new Date('01/01/1950')),
+    new Person(2,'Bernie','Monnete',new Date('01/01/1955')),
+    new Person(3,'Sean','Doyle',new Date('01/01/1955'))
+  ];
+
+  getPeople(): Observable<Person[]>{
+    return of(this.people);
+  }
+
+  constructor() { }
+}
